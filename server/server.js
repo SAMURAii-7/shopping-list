206fb5c009b63da738e675b99230f08665f3f7b8
SAMURAii-7 <shubham.prasad21@gmail.com>
2022-10-26 03:48:13 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -13,11 +13,7 @@ const verify = require("./routes/verifyToken");
 
 const app = express();
 
-app.use(
-    cors({
-        origin: "https://shopping-list-ui.onrender.com/",
-    })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
