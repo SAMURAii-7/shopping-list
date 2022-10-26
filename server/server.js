42bba225f0b7c714b5796a44ebf7fda29ce5d157
SAMURAii-7 <shubham.prasad21@gmail.com>
2022-10-26 13:22:20 +0530

    cors allow all origins fix

--- server/server.js
+++ server/server.js
@@ -13,7 +13,8 @@ const verify = require("./routes/verifyToken");
 
 const app = express();
 
-app.use(cors());
+// app.use(cors()); use in production
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
