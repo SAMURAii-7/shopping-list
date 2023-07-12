be084973fb8c833c3faab059a8badbcbcd218656
Shubham Prasad <61153966+SAMURAii-7@users.noreply.github.com>
2023-07-12 21:24:15 +0530

    add port and secure in mail.js

--- server/mail.js
+++ server/mail.js
@@ -3,6 +3,8 @@ const nodemailer = require("nodemailer");
 module.exports = function sendEmail(email, link) {
     const transporter = nodemailer.createTransport({
         service: "gmail",
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.SENDER_MAIL,
             pass: process.env.SENDER_PASS,
