fca35046dc1de4b1f1cd8e20e67ace0ba075936d
Shubham Prasad <61153966+SAMURAii-7@users.noreply.github.com>
2023-10-23 02:59:22 +0530

    add s3 bucket name

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const compression = require("compression");
 const ObjectsToCsv = require("objects-to-csv");
-const fs = require("@cyclic.sh/s3fs");
+const fs = require("@cyclic.sh/s3fs")(
+    "cyclic-tiny-tan-piranha-sari-ap-south-1"
+);
 require("dotenv").config();
 
 //import routes
