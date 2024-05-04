6c41046e4d6d66c189588ab5eef68ae4bf570bb4
SAMURAii-7 <shubham.prasad21@gmail.com>
2024-05-05 02:26:57 +0530

    migrate from nodemailer to sendgrid

--- server/mail.js
+++ server/mail.js
@@ -1,33 +1,22 @@
-const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail')
 require("dotenv").config();
 
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 module.exports = function sendEmail(email, link) {
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.SENDER_MAIL,
-            pass: process.env.SENDER_PASS,
-        },
-    });
 
-    const details = {
-        from: process.env.SENDER_MAIL,
+    const msg = {
         to: email,
-        subject: "Password Reset Link",
+        from: 'projects@shubhamprasad.dev',
+        subject: 'Password Reset Link',
         text: "Password Reset Link: " + link,
-        html:
-            "<h1>Password Reset Link</h1><p>Click on the link to reset your password: <a href=" +
-            link +
-            ">Reset Password</a></p>",
-    };
-
-    transporter.sendMail(details, (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Email sent successfully");
-        }
-    });
+        html: "<h1>Password Reset Link</h1><p>Click on the link to reset your password: <a href=" + link + ">Reset Password</a></p>",
+    }
+    sgMail
+        .send(msg)
+        .then(() => {
+            console.log('Email sent')
+        })
+        .catch((error) => {
+            console.error(error)
+        })
 };
